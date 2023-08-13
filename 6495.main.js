0074b6769372c138e611333ac1d1c1762389487e
Talon <talon.nguyen@f45hqconsultant.com>
2023-08-13 19:35:26 +0700

    add tp.ent

--- 6495.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkreact_micro_frontend=self.webpackChunkreact_micro_frontend||[]).push([[6495],{4970:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
